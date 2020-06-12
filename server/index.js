86ae7ff04dff54443d61d7e97e8f7984b434e7b7
Justin Nguyen <Vu@Justins-MBP.attlocal.net>
2020-06-12 18:09:53 -0500

    install dependencies, reorganize file structure, add components

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http'); 
+
+const PORT = 4000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => {
+    console.log(`Server has started on Port ${PORT}`);
+});
